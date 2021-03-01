301bd42dfc4ad5c9c5d9ca9aba46611db09fd4a8
paul <paul.18.sma@gmail.com>
2021-03-01 11:27:20 -0300

    set register path as public

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import authController from '../controllers/auth.controller';
+
 class PublicRoutes{
 
   constructor(private router: Router = Router()){}
@@ -6,6 +8,7 @@ class PublicRoutes{
   // deefine your public routes inside of routes function
   public routes(): Router{
     // this.router.get('home', (req: Request, res: Response): Response => { return res.send('Welcome home') } ) // example
+    this.router.post('/auth/register', authController.register);
     return this.router;
   }
 }
